62af2e72dddc8f4955801d13e59323e2fe18bee3
Zack Brashear <zackbrashear@gmail.com>
2025-09-28 16:16:59 -0400

    added functionality to display individual lost

--- todos.js
+++ todos.js
@@ -2,7 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const flash = require("express-flash");
 const session = require("express-session");
-const TodoList = require("./lib/todolist.js")
+const { body, validationResult } = require("express-validator");
+const TodoList = require("./lib/todolist")
+const { sortTodoLists, sortTodos } = require("./lib/sort");
 
 
 const app = express();
@@ -10,6 +12,7 @@ const host = "localhost";
 const port = 3000;
 
 let todoLists = require("./lib/seed-data");
+const Todo = require("./lib/todo.js");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -25,31 +28,17 @@ app.use(session({
 }));
 
 app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+})
+
+const loadTodoList = todoListId => {
+  return todoLists.find(list => list.id === todoListId);
+}
 
 
-const sortTodoLists = lists => {
-  return lists.slice().sort((todoListA, todoListB) => {
-    let isDoneA = todoListA.isDone();
-    let isDoneB = todoListB.isDone();
-
-    if (!isDoneA && isDoneB) {
-      return -1;
-    } else if (isDoneA && !isDoneB) {
-      return 1;
-    } else {
-      let titleA = todoListA.title.toLowerCase();
-      let titleB = todoListB.title.toLowerCase();
-
-      if (titleA < titleB) {
-        return -1;
-      } else if (titleA > titleB) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-  });
-};
 
 app.get("/", (req, res) => {
   res.redirect("/lists");
@@ -80,7 +69,7 @@ app.post("/lists", (req, res) => {
       todoListTitle: req.body.todoListTitle,
     })
   } else if (todoLists.some(todoList => todoList.title === title)) {
-    req.flash("error", "Title must be unique,");
+    req.flash("error", "Title must be unique.");
     res.render("new-list", {
       flash: req.flash(),
       todoListTitle: req.body.todoListTitle,
@@ -92,7 +81,58 @@ app.post("/lists", (req, res) => {
   }
 })
 
+app.post("/lists", (req, res) => {
+  [
+    body("todoListTitle")
+      .trim()
+      .isLength({min: 1})
+      .withMessage("Title is Required")
+      .bail()
+      .isLength({max: 100})
+      .withMessage("Title cannot exceed 100 characters")
+      .custom(title => {
+        let duplicate = todoLists.find(list => list.title = title);
+        return duplicate === undefined;
+      })
+      .withMessage("That todo alreadt exists")
+  ],
+  (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      errors.array().forEach(err => req.flash("error", err.msg));
+      res.render("new-list", {
+        flash: req.flash(),
+        todoListTitle: req.body.todoListTitle,
+      });
+    } else {
+      todoLists.push(new TodoList(req.body.todoListTitle));
+      req.flash("success", "New Todo Created!")
+      res.redirect("/lists")
+    }
+  }
+})
+
+app.get("/lists/:todoListId", (req, res, next) => {
+  let todoListId = req.params.todoListId;
+  let selectedTodoList = loadTodoList(+todoListId);
+
+  if (selectedTodoList === undefined) {
+    next(new Error("Not found."));
+  } else {
+    res.render("list", {
+      todoList: selectedTodoList,
+      todos: sortTodos(selectedTodoList),
+    })
+  }
+});
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
+});
+
 app.listen(port, host, () => {
   console.log("listening on port " + port + " of " + host + "...");
 });
+
 
